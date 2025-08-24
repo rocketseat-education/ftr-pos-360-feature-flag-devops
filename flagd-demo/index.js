e2978d5a159f345ea476e805b7b98fa396cad882
EgydioBNeto <egydiobolonhezi@gmail.com>
2025-08-24 15:45:48 -0300

    Criando o arquivo de definição de flags

--- flagd-demo/index.js
+++ flagd-demo/index.js
@@ -1,12 +1,18 @@
 import { OpenFeature } from "@openfeature/server-sdk";
 import { FlagdProvider } from "@openfeature/flagd-provider";
 
+const FLAG = 'LOGIN_PAGE';
+
 const provider = new FlagdProvider({
     host: "127.0.0.1",
     port: 8014
 });
 
 (async () => { 
-    await OpenFeature.setProvider(provider);
+    await OpenFeature.setProviderAndWait(provider);
     const client = await OpenFeature.getClient();
+    const flag = await client.getBooleanValue(FLAG, false);
+    const details = await client.getBooleanDetails(FLAG, false);
+    console.log(flag);
+    console.log(details);
 })();
