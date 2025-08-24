4c3cf685e47068893a2040e8f10dd962c5c5f016
EgydioBNeto <egydiobolonhezi@gmail.com>
2025-08-24 15:39:05 -0300

    Colocando o flagd pra rodar via container

--- flagd-demo/index.js
+++ flagd-demo/index.js
@@ -1,7 +1,10 @@
 import { OpenFeature } from "@openfeature/server-sdk";
 import { FlagdProvider } from "@openfeature/flagd-provider";
 
-const provider = new FlagdProvider();
+const provider = new FlagdProvider({
+    host: "127.0.0.1",
+    port: 8014
+});
 
 (async () => { 
     await OpenFeature.setProvider(provider);
